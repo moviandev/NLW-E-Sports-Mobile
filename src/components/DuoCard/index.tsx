a2d9ade194ca0e3f4907d940c4983f1ba701c97f
Matheus Viana <matheus_o_viana@hotmail.com>
2022-09-17 00:20:39 -0300

    added touchableOpacity for conecte-se button and added DuoInfo for "Chamada de audio"

--- src/components/DuoCard/index.tsx
+++ src/components/DuoCard/index.tsx
@@ -1,7 +1,10 @@
-import { View } from 'react-native';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { GameController } from 'phosphor-react-native';
+
 import { DuoInfo } from '../DuoInfo';
 
 import { styles } from './styles';
+import { THEME } from '../../theme';
 
 export interface DuoCardProps {
   hourEnd: string;
@@ -10,13 +13,15 @@ export interface DuoCardProps {
   name: string;
   weekDays: string[],
   yearsPlaying: number,
+  useVoiceChannel: boolean,
 }
 
 interface Props {
   data: DuoCardProps;
+  onConnect: () => void;
 }
 
-export function DuoCard({ data }: Props) {
+export function DuoCard({ data, onConnect }: Props) {
   return (
     <View style={styles.container}>
       <DuoInfo
@@ -31,8 +36,28 @@ export function DuoCard({ data }: Props) {
 
       <DuoInfo
         label="Disponibilidade"
-        value={`${data.weekDays.length} dias`}
+        value={`${data.weekDays.length} dias \u2022 ${data.hoursStart} - ${data.hourEnd}`}
       />
+
+      <DuoInfo
+        label="Chamada de áudio?"
+        value={data.useVoiceChannel ? 'Sim' : 'Não'}
+        colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onConnect}
+      >
+        <GameController
+          color={THEME.COLORS.TEXT}
+          size={20}
+        />
+
+        <Text style={styles.buttonTitle}>
+          Conectar
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
