ac8c5ef1cbfa892032d30d22431cd7d484742e74
Matheus Viana <matheus_o_viana@hotmail.com>
2022-09-18 21:53:26 -0300

    added structure to recive notification

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import {
   useFonts,
@@ -6,19 +7,48 @@ import {
   Inter_700Bold,
   Inter_900Black
 } from '@expo-google-fonts/inter';
+import { Subscription } from 'expo-modules-core'
+import * as Notifications from 'expo-notifications';
+
 
 import { Loader } from './src/components/Loader';
 import { Background } from './src/components/Background';
 import { Routes } from './src/routes';
 
+import './src/services/notificationConfig';
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
+
 export default function App() {
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener(n => { });
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener(r => { });
+
+    return () => {
+      if (getNotificationListener.current && responseNotificationListener.current) {
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+
+  }, []);
+
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
     Inter_900Black
   });
-
+  // Adicionar notificoes
   return (
     <Background>
       <StatusBar
